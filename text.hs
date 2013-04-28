4ce6467c10d636c1e72a01fecc2030ddd7fb4ecd
GrimChains <grimchains@gmail.com>
2013-04-27 20:53:58 -0400

    Now we have TWO lines!

--- text.hs
+++ text.hs
@@ -1,22 +1,30 @@
 import Graphics.UI.SDL 			as SDL
 import Graphics.UI.SDL.TTF 		as SDLt
---import Graphics.UI.SDL.Color		--as SDLc
 import Data.Word
 import Graphics.UI.SDL.General		as SDLg
 
 main ::IO ()
 main = do
+	-- init everything
 	SDL.init [SDL.InitEverything]
+	SDLt.init
+
+	-- set up window size
 	SDL.setVideoMode 600 600 32 []
-	SDL.setCaption "Video Test!" "video test"
 	
 	mainSurf <- getVideoSurface
 	SDLt.init
 	font <- openFont "FreeMono.ttf" 12
 	let color = (Color 255 0 0)
 	let rect = Just (Rect 0 0 300 300)
+	let offset = Just (Rect 0 0 0 0)
 	text <- renderTextSolid font "hello world!" color
-	blitSurface text rect mainSurf Nothing
+	blitSurface text rect mainSurf offset
+	SDL.flip mainSurf
+
+	let offset = Just (Rect 0 12 0 0)
+	text <- renderTextSolid font "hello again!" color
+	blitSurface text rect mainSurf offset
 	SDL.flip mainSurf
 
 	eventLoop
