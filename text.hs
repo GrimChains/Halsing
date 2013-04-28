82a9fad3dd86acc75863d06afd2aa2dc5a031147
GrimChains <grimchains@gmail.com>
2013-04-27 21:25:34 -0400

    Cleaned up code

--- text.hs
+++ text.hs
@@ -1,6 +1,5 @@
 import Graphics.UI.SDL 			as SDL
 import Graphics.UI.SDL.TTF 		as SDLt
-import Data.Word
 import Graphics.UI.SDL.General		as SDLg
 
 main ::IO ()
@@ -11,18 +10,21 @@ main = do
 
 	-- set up window size
 	SDL.setVideoMode 600 600 32 []
-	
-	mainSurf <- getVideoSurface
-	SDLt.init
-	font <- openFont "FreeMono.ttf" 12
-	let color = (Color 255 0 0)
-	let rect = Just (Rect 0 0 300 300)
+
+	-- set up constants
+	let size	= 12
+	let color	= (Color 255 0 0)
+	let rect	= Just (Rect 0 0 600 600)
+	mainSurf	<- getVideoSurface
+	font		<- openFont "FreeMono.ttf" size
+
+	-- build text lines
 	let offset = Just (Rect 0 0 0 0)
 	text <- renderTextSolid font "hello world!" color
 	blitSurface text rect mainSurf offset
 	SDL.flip mainSurf
 
-	let offset = Just (Rect 0 12 0 0)
+	let offset = Just (Rect 0 size 0 0)
 	text <- renderTextSolid font "hello again!" color
 	blitSurface text rect mainSurf offset
 	SDL.flip mainSurf
