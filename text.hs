5400803433bf24adadb5fb6e8ecce0851f4a231c
GrimChains <grimchains@gmail.com>
2013-05-02 00:13:35 -0400

    Cleaned up text.hs and added in a text rendering function.

--- text.hs
+++ text.hs
@@ -19,15 +19,9 @@ main = do
 	font		<- openFont "FreeMono.ttf" size
 
 	-- build text lines
-	let offset = Just (Rect 0 0 0 0)
-	text <- renderTextSolid font "hello world!" color
-	blitSurface text rect mainSurf offset
-	SDL.flip mainSurf
-
-	let offset = Just (Rect 0 size 0 0)
-	text <- renderTextSolid font "hello again!" color
-	blitSurface text rect mainSurf offset
-	SDL.flip mainSurf
+	buildText "Line 1" 0 size font mainSurf color
+	buildText "Line 2" 1 size font mainSurf color
+	buildText "Line 3" 2 size font mainSurf color
 
 	eventLoop
 	SDL.quit
@@ -36,3 +30,17 @@ main = do
 		eventLoop = SDL.waitEventBlocking >>= checkEvent
 		checkEvent (KeyUp _)	= return ()
 		checkEvent _		= eventLoop
+
+
+-- Message -> Pixel offset -> Text font -> Base Surface -> Text color -> Output
+buildText :: String -> Int -> Int -> Font -> Surface -> Color -> IO()
+buildText message line size font mainSurf color = do
+	-- set up constants
+	let offset	= Just (Rect 0 (size * line) 0 0)
+	let rect	= Just (Rect 0 0 600 600)
+
+	-- render our text
+	text <- renderTextSolid font message color
+	blitSurface text rect mainSurf offset
+	SDL.flip mainSurf
+	return()
